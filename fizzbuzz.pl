0c5d1537c898f5b286b7fe76888bc89666e681e3
Jeffrey Kegler <jeffreykegler@cpan.org>
2018-10-20 19:53:11 -0400

    Work on fizzbuzz

--- fizzbuzz.pl
+++ fizzbuzz.pl
@@ -30,7 +30,7 @@ $(count (add 1 count))
 EOI
 
 my $dsl = <<'EODSL';
-:default ::= action => ::first
+:default ::= action => [name,start,length,values]
 lexeme default = latm => 1
 
 top ::= leader hoon trailer
@@ -41,17 +41,56 @@ ws_element ::= ACE
 ws_element ::= gap
 ws_element ::= COMMENT
 
+hoon ::= tall_barhep
 hoon ::= tall_bartis
+hoon ::= tall_cenhep
+hoon ::= irr_censig
+hoon ::= irr_centis
+hoon ::= tall_colhep
+hoon ::= irr_dottis
+hoon ::= tall_kethep
+hoon ::= irr_kettis
 hoon ::= tall_tislus
+hoon ::= tall_wutcol
 hoon ::= slash_2cell
+hoon ::= noun
 
-tall_bartis ::= BARTIS gap model gap value
-value ::= noun # probably should not be hoon
-model ::= noun # probably should not be hoon
-noun ::= hoon
 noun ::= NAME
+noun ::= NUMBER
+noun ::= STRING
+noun ::= TERM
+noun ::= NIL
+toga ::= NAME # TODO: is this right?
+
+tall_barhep ::= BARHEP gap hoon
+
+tall_bartis ::= BARTIS gap hoon gap hoon
+
+tall_cenhep ::= CENHEP gap hoon gap hoon
+
+# It's hard to find an exact description of this
+# in the docs, but the various arities seem to
+# all be irregular variations of censig.
+irr_censig ::= '(' hoonAceSeq ')'
+hoonAceSeq ::= hoon+ separator=>ACE proper=>1
+
+# Apparently a form of centis with the special
+# limb $ for the recursion point
+irr_centis ::= '$(' hoonAceSeq ')'
+
+tall_colhep ::= COLHEP gap hoon gap hoon
+
+irr_dottis ::= '=(' hoon ACE hoon ')'
+
+tall_kethep ::= KETHEP gap hoon gap hoon
+
+irr_kettis ::= toga '=' hoon
 
 tall_tislus ::= TISLUS gap hoon gap hoon
+
+tall_wutcol ::= WUTCOL gap hoon gap hoon gap hoon
+
+slash_2cell ::= NAME '/' NAME
 
 gap ::= ACE ACE optAces
 gap ::= optAces NL optAces
@@ -61,18 +100,82 @@ optAces ::= aces
 aces ::= ACE*
 comments ::= COMMENT*
 
+backslash ~ [\0x5c] # 0x5c is backslash
+
 ACE ~ ' '
 COMMENT ~ '::' nonNLs nl
 NL ~ nl
-nl ~ '\n'
+nl ~ [\n]
 nonNLs ~ nonNL*
 nonNL ~ [^\n]
-NAME ~ [\w]+
+NAME ~ nameFirstChar nameLaterChars
 
+nameFirstChar ~ [_a-zA-Z]
+nameLaterChars ~ nameLaterChar*
+nameLaterChar ~ [\w]
+
+NIL ~ '~'
+
+BARHEP ~ '|-'
 BARTIS ~ '|='
+CENHEP ~ '%-'
+COLHEP ~ ':-'
+KETHEP ~ '^-'
 TISLUS ~ '=+'
+WUTCOL ~ '?:'
 
-slash_2cell ::= NAME '/' NAME
+wsChars ~ wsChar*
+wsChar ~ [ \n]
+
+NUMBER ~ decimalNumber
+NUMBER ~ hexNumber
+NUMBER ~ binaryNumber
+decimalNumber ~ decimalPrefix decimalGroups
+decimalPrefix ~ decimalDigit
+decimalPrefix ~ decimalDigit decimalDigit
+decimalPrefix ~ decimalDigit decimalDigit decimalDigit
+decimalDigit ~ [0-9]
+decimalGroups ~ decimalGroup*
+decimalGroup ~ [.] wsChars decimalDigit decimalDigit decimalDigit
+
+hexNumber ~ '0x' hexPrefix hexGroups
+hexPrefix ~ hexDigit
+hexPrefix ~ hexDigit hexDigit
+hexPrefix ~ hexDigit hexDigit hexDigit
+hexPrefix ~ hexDigit hexDigit hexDigit hexDigit
+hexDigit ~ [0-9a-fA-F]
+hexGroups ~ hexGroup*
+hexGroup ~ [.] wsChars hexDigit hexDigit hexDigit hexDigit
+
+binaryNumber ~ '0b' binaryPrefix binaryGroups
+binaryPrefix ~ binaryDigit
+binaryPrefix ~ binaryDigit binaryDigit
+binaryPrefix ~ binaryDigit binaryDigit binaryDigit
+binaryPrefix ~ binaryDigit binaryDigit binaryDigit binaryDigit
+binaryDigit ~ [01]
+binaryGroups ~ binaryGroup*
+binaryGroup ~ [.] wsChars binaryDigit binaryDigit binaryDigit binaryDigit
+
+# syn match       hoonNumber        "\d\{1,3\}\%(\.\_s\?\d\{3\}\)*"
+# syn match       hoonNumber        "0x\x\{1,4\}\%(\.\_s*\x\{4\}\)*"
+# syn match       hoonNumber        "0b[01]\{1,4\}\%(\.\_s*[01]\{4\}\)*"
+# syn match       hoonNumber        "0v[0-9a-v]\{1,5\}\%(\.\_s*[0-9a-v]\{5\}\)*"
+# syn match       hoonNumber        "0w[-~0-9a-zA-Z]\{1,5\}\%(\.\_s*[-~0-9a-zA-Z]\{5\}\)*"
+
+STRING ~ ["] doubleStringElements ["]
+doubleStringElements ~ doubleStringElement*
+# 0x5C is backslash
+# From syntax.vim, might need correction
+doubleStringElement ~ [^"\x5c] | backslash ["] | backslash backslash
+
+# syn region      hoonString        start=+'+ skip=+\\[\\']+ end=+'+ contains=@spell
+# syn region      hoonBlock         start=+'''+ end=+'''+
+# syn region      hoonString        start=+"+ skip=+\\[\\"]+ end=+"+ contains=@spell
+
+# From syntax.vim, probably need correction
+TERM ~ '%' termChar termChars
+termChars ~ termChar*
+termChar ~ [-\w]
 
 EODSL
 
@@ -80,16 +183,23 @@ my $grammar = Marpa::R2::Scanless::G->new( { source => \$dsl } );
 
 TEST: {
     my $input = \$fizzbuzz;
-    my $recce = Marpa::R2::Scanless::R->new( { grammar => $grammar } );
-    my $value_ref;
-    my $eval_ok = eval { $value_ref = doit( $recce, $input ); 1; };
+    my $recce = Marpa::R2::Scanless::R->new( { grammar => $grammar,
+       trace_lexers => 1,
+       trace_terminals => 1,
+    } );
+    my $valueRef;
+    my $eval_ok = eval { $valueRef = doit( $recce, $input ); 1; };
     if ( !$eval_ok ) {
         Test::More::fail('Example 3 failed');
         Test::More::diag($EVAL_ERROR);
         last TEST;
     }
-    my $result = [ grep {defined} split q{ }, ${$input} ];
-    Test::More::is_deeply( $result, flatten($value_ref), 'Example 3' );
+    # my $result = [ grep {defined} split q{ }, ${$input} ];
+    local $Data::Dumper::Deepcopy    = 1;
+    local $Data::Dumper::Terse    = 1;
+
+    say Data::Dumper::Dumper($valueRef);
+    # Test::More::is_deeply( $result, $valueRef, 'Example 3' );
 } ## end TEST:
 
 sub doit {
@@ -114,10 +224,10 @@ sub doit {
             Marpa::R2::Internal::ASF::ambiguities_show( $asf, \@ambiguities );
     } ## end if ( $recce->ambiguity_metric() > 1 )
 
-    my $value_ref = $recce->value();
-    if ( !$value_ref ) {
+    my $valueRef = $recce->value();
+    if ( !$valueRef ) {
         die "input read, but there was no parse";
     }
 
-    return $value_ref;
+    return $valueRef;
 } ## end sub doit
