fa31cc1108b5d18650f7866cc161c27d64707be7
Jeffrey Kegler <jeffreykegler@cpan.org>
2019-05-02 20:39:34 -0400

    Suppress warnings from pending issues

---
+++ edit_later.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+TMP=junk.$$.1
+TARGET=Lint/later.suppressions
+sed -e '/### PENDING ISSUES AFTER HERE$/q' $TARGET > $TMP
+for f in issues/*.include
+do
+echo >> $TMP
+echo === $f === >> $TMP
+cat $f >> $TMP
+done
+mv $TMP $TARGET
