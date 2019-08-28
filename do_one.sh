e2af4ea76a92b4c14ff981f917d2c72f00ce8f0e
Jeffrey Kegler <jeffreykegler@cpan.org>
2019-08-28 17:00:33 -0400

    Clean up directory structure

--- do_one.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-F=hoons/arvo/"$1"
-if ! test -r $F
-then
-    echo $F is not readable 1>&2
-    exit 1
-fi
-perl -I. -ILint Lint/hoonlint.pl -S Lint/anomaly.suppressions -S Lint/later.suppressions $F
--- one_diff.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set +o posix
-if ! test -r hoons/arvo/"$1"
-then
-    echo hoons/arvo/"$1" is not readable 1>&2
-    exit 1
-fi
-perl -I. -ILint Lint/hoonlint.pl -S Lint/anomaly.suppressions -S Lint/later.suppressions hoons/arvo/"$1" |
-diff - <(egrep "$1" Lint/arvo.lint.out)
