b0637f24f0c564bf421c2cab27856cda222b1f7e
Jeffrey Kegler <jeffreykegler@cpan.org>
2019-08-30 18:28:14 -0400

    Bump version number

--- cpan/lib/MarpaX/Hoonlint.pm
+++ cpan/lib/MarpaX/Hoonlint.pm
@@ -15,7 +15,7 @@ use Getopt::Long;
 use MarpaX::Hoonlint::yahc;
 
 use vars qw($VERSION $STRING_VERSION @ISA $DEBUG);
-$VERSION        = '0.001_001';
+$VERSION        = '1.001_002';
 $STRING_VERSION = $VERSION;
 ## no critic (BuiltinFunctions::ProhibitStringyEval)
 $VERSION = eval $VERSION;
